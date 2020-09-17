0ef6f201fd065a6e99ab201c2d9c8efc33af434e
hojooneum <bldolphin96@gmail.com>
2020-09-17 19:24:52 +0900

    [ADD] flow(정적타입언어) 설정 완료

---
+++ src/components/Home.component.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { SafeAreaView, View, Platform } from "react-native";
+import { Button, Divider, Layout, TopNavigation } from "@ui-kitten/components";
+import { useSafeArea } from "react-native-safe-area-context";
+import { ThemeContext } from "../theme-context";
+
+export const HomeScreen = ({ navigation }) => {
+  const navigateDetails = () => {
+    navigation.navigate("Details");
+  };
+
+  const themeContext = React.useContext(ThemeContext);
+
+  const inset = useSafeArea();
+
+  return (
+    <SafeAreaView
+      style={{ flex: 1, paddingTop: Platform.OS === "android" ? inset.top : 0 }}
+    >
+      <TopNavigation title="MyApp" alignment="center" />
+      <Divider />
+      <Layout
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        <Button style={{ marginVertical: 4 }} onPress={navigateDetails}>
+          OPEN DETAILS
+        </Button>
+        <Button
+          style={{ marginVertical: 4 }}
+          onPress={themeContext.toggleTheme}
+        >
+          TOGGLE THEME
+        </Button>
+      </Layout>
+    </SafeAreaView>
+  );
+};
