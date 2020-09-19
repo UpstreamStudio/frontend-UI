458d0e6a67681512cde95bea739c53a8b3e3a10c
hojooneum <bldolphin96@gmail.com>
2020-09-19 18:25:53 +0900

    [UPDATE] 로그인폼 레이아웃 수정

--- src/components/TextButton.component.js
+++ src/components/TextButton.component.js
@@ -1,12 +1,27 @@
 // @flow
 import React from "react";
-import { Button } from "@ui-kitten/components";
+import { Button, Text } from "@ui-kitten/components";
 
+const style = {
+  text: {
+    fontSize: 20,
+    color: "#fff",
+    fontWeight: "bold",
+  },
+  button: {
+    height: 60,
+  },
+};
 export const TextButton = (props: Object): any => {
   const { text, appearance, status, size, ...others } = props;
   return (
-    <Button appearance={appearance} status={status} size={size}>
-      {text}
+    <Button
+      style={style.button}
+      appearance={appearance}
+      status={status}
+      size={size}
+    >
+      <Text style={style.text}>{text}</Text>
     </Button>
   );
 };
